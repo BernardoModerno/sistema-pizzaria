4a54696dc651f4e18896d2b83045060f86eea1b0
BernardoModerno <bernardo.moderno@gmail.com>
2022-06-11 22:48:57 -0300

    Concluindo pedido

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -72,6 +72,27 @@ export default function Dashboard({ orders }: HomeProps){
   }
 
 
+  async function handleFinishItem(id: string){
+    const apiClient = setupAPIClient();
+    await apiClient.put('/order/finish', {
+      order_id: id,
+    })
+
+    const response = await apiClient.get('/orders');
+
+    setOrderList(response.data);
+    setModalVisible(false);
+  }
+
+
+  async function handleRefreshOrders(){
+    const apiClient = setupAPIClient();
+
+    const response = await apiClient.get('/orders')
+    setOrderList(response.data);
+
+  }
+
   Modal.setAppElement('#__next');
 
   return(
@@ -86,12 +107,18 @@ export default function Dashboard({ orders }: HomeProps){
 
         <div className={styles.containerHeader}>
           <h1>Últimos pedidos</h1>
-          <button>
+          <button onClick={handleRefreshOrders}>
             <FiRefreshCcw size={25} color="#3fffa3"/>
           </button>
         </div>
 
         <article className={styles.listOreders}>
+
+          {orderList.length === 0 && (
+            <span className={styles.emptyList}>
+              Nenhum pedido aberto foi encontrado...
+            </span>
+          )}
 
           {orderList.map( item => (
             <section  key={item.id} className={styles.orderItem}> 
@@ -111,6 +138,7 @@ export default function Dashboard({ orders }: HomeProps){
           isOpen={modalVisible}
           onRequestClose={handleCloseModal}
           order={modalItem}
+          handleFinishOrder={ handleFinishItem }
         />
       )}
 
