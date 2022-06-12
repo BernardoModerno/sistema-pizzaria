ddf8e99a45fb8de8191f9ebd17a1835bf7d4d868
BernardoModerno <bernardo.moderno@gmail.com>
2022-06-12 15:44:51 -0300

    Configurando navegação

---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {View} from 'react-native';
+
+import AppRoutes from './app.routes';
+import AuthRoutes from './auth.routes';
+
+function Routes(){
+  const isAuthenticated = true;
+  const loading = false;
+
+
+  return(
+    isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
+  )
+}
+
+
+export default Routes;
