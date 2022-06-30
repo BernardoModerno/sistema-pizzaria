80f25c442b90fe89c758944e3c8d1d64347b5701
BernardoModerno <bernardo.moderno@gmail.com>
2022-06-29 22:19:31 -0300

    Itens de um pedido

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3333'
-  baseURL: 'http://192.168.15.7:3333'
+  baseURL: 'http://172.30.176.1:3333'
 })
 
 export { api };
